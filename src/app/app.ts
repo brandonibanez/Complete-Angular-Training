8bdfe4489d36e01d3a3da7c8af279802aa947c20
brandonibanez <ibanezbrandonn@gmail.com>
2025-06-29 22:51:19 +0800

    Include header and user list to display in web.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header';
+import { User } from "./user/user";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, User],
   templateUrl: './app.html',
   styleUrl: './app.sass'
 })
