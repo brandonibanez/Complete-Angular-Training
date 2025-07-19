abe8e4d0583d5ab9d877a7d9de5913a16bf4165a
Brandon Ibañez <ibanezbrandonn@gmail.com>
2025-07-19 14:19:18 +0800

    Signals with service

--- src/app/investment.service.ts
+++ src/app/investment.service.ts
@@ -1,17 +1,17 @@
-import { Injectable } from "@angular/core";
+import { Injectable, signal } from "@angular/core";
 import { InvestmentInput } from "./investment-input.model";
 
 @Injectable({providedIn: 'root'})
 export class InvestmentService {
 
-    resultData?: {
+    resultData = signal<{
         year: number,
         interest: number,
         valueEndOfYear: number,
         annualInvestment: number,
         totalInterest: number,
         totalAmountInvested: number,
-      }[];
+      }[] | undefined>(undefined);
 
     calculateInvestmentResults(data: InvestmentInput) {
         const { initialInvestment, duration, expectedReturn, annualInvestment } = data;
@@ -33,7 +33,7 @@ export class InvestmentService {
             totalAmountInvested: initialInvestment + annualInvestment * year,
           });
         }
-        this.resultData = annualData;
+        this.resultData.set(annualData);
       }
 
 }
