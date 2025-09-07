c73f8cc8dde949fc82b089789bd2326919a55065
Brandon Ibañez <ibanezbrandonn@gmail.com>
2025-09-07 12:21:17 +0800

    Section 13
    Handling user inputs & working with forms

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,5 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
-
-bootstrapApplication(AppComponent, {
-    providers: [provideHttpClient(
-        withInterceptors([])
-    )],
-}).catch((err) => console.error(err));
+bootstrapApplication(AppComponent).catch((err) => console.error(err));
