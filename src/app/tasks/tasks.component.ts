8c2ab552cae4444ea1d98b49821d949dcdfad665
Brandon Ibañez <ibanezbrandonn@gmail.com>
2025-09-13 22:38:25 +0800

    Filter with Query Parameters

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,4 +1,4 @@
-import { Component, DestroyRef, OnInit, computed, inject, input } from '@angular/core';
+import { Component, DestroyRef, OnInit, computed, inject, input, signal } from '@angular/core';
 
 import { TaskComponent } from './task/task.component';
 import { TasksService } from './tasks.service';
@@ -15,11 +15,20 @@ import { map } from 'rxjs';
 export class TasksComponent implements OnInit {
   userId = input.required<string>();
   // order = input<'asc' | 'desc'>();
-  order?: 'asc' | 'desc';
+  order = signal<'asc' | 'desc'>('desc');
   private tasksService = inject(TasksService);
 
   userTasks = computed(() =>
-    this.tasksService.allTasks().filter((task) => task.userId === this.userId())
+    this.tasksService.
+      allTasks().
+      filter((task) => task.userId === this.userId()).
+      sort((a,b) => {
+        if(this.order() === 'desc') {
+          return a.id > b.id ? -1 : 1;
+        } else {
+          return a.id > b.id ? 1 : -1;
+        }
+      })
   );
 
   private activatedRoute = inject(ActivatedRoute);
@@ -27,7 +36,7 @@ export class TasksComponent implements OnInit {
 
   ngOnInit(): void {
     const subscription = this.activatedRoute.queryParams.subscribe({
-      next: params => this.order = params['order']
+      next: params => this.order.set(params['order'])
     })
 
     this.destroyRef.onDestroy(() => subscription.unsubscribe());
