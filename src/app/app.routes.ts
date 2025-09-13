8f5a5652f66b75bff16d2497b87b231a3bee02e9
Brandon Ibañez <ibanezbrandonn@gmail.com>
2025-09-13 17:35:59 +0800

    Multiple Routes Files

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,8 @@
 import { Routes } from '@angular/router';
 import { NoTaskComponent } from './tasks/no-task/no-task.component';
 import { UserTasksComponent } from './users/user-tasks/user-tasks.component';
-import { NewTaskComponent } from './tasks/new-task/new-task.component';
-import { TasksComponent } from './tasks/tasks.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { routes as userRoutes } from './users/users.route';
 
 export const routes: Routes = [
   {
@@ -13,21 +12,7 @@ export const routes: Routes = [
   {
     path: 'users/:userId',
     component: UserTasksComponent,
-    children: [
-      {
-        path: '',
-        redirectTo: 'tasks',
-        pathMatch: 'full'
-      },
-      {
-        path: 'tasks',
-        component: TasksComponent,
-      },
-      {
-        path: 'tasks/new',
-        component: NewTaskComponent,
-      },
-    ],
+    children: userRoutes
   },
   {
     path: '**',
