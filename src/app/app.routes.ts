f893560c08fd6f1ee53648fff7347280d65df7e6
Brandon Ibañez <ibanezbrandonn@gmail.com>
2025-09-20 19:12:50 +0800

    Route Guards

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -24,6 +24,7 @@ export const routes: Routes = [
     path: 'users/:userId',
     component: UserTasksComponent,
     children: userRoutes,
+    canMatch: [dummyCanMatch],
     data: {
       message: 'Hello'
     },
