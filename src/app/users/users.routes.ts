b46cd49a52116e51de2defd2fa7b1bb5528301f4
Brandon Ibañez <ibanezbrandonn@gmail.com>
2025-10-05 13:43:23 +0800

    Section 16
    Starter project
    Deploying Angular Apps - CSR, SSR, SGA

--- src/app/users/users.routes.ts
+++ src/app/users/users.routes.ts
@@ -1,37 +1,25 @@
 import { Routes } from '@angular/router';
 
-import {
-  NewTaskComponent,
-  canLeaveEditPage,
-} from '../tasks/new-task/new-task.component';
-import { TaskComponent } from '../tasks/task/task.component';
-import { resolveUserTasks } from '../tasks/tasks.component';
-import { TasksService } from '../tasks/tasks.service';
+import { TasksComponent, resolveUserTasks } from '../tasks/tasks.component';
+import { NewTaskComponent, canLeaveEditPage } from '../tasks/new-task/new-task.component';
 
 export const routes: Routes = [
   {
     path: '',
-    providers: [TasksService],
-    children: [
-      {
-        path: '',
-        redirectTo: 'tasks',
-        pathMatch: 'full',
-      },
-      {
-        path: 'tasks', // <your-domain>/users/<uid>/tasks
-        component: TaskComponent,
-        // loadComponent: () => import('../tasks/tasks.component').then(mod => mod.TasksComponent),
-        runGuardsAndResolvers: 'always',
-        resolve: {
-          userTasks: resolveUserTasks,
-        },
-      },
-      {
-        path: 'tasks/new',
-        component: NewTaskComponent,
-        canDeactivate: [canLeaveEditPage],
-      },
-    ]
+    redirectTo: 'tasks',
+    pathMatch: 'full',
+  },
+  {
+    path: 'tasks', // <your-domain>/users/<uid>/tasks
+    component: TasksComponent,
+    runGuardsAndResolvers: 'always',
+    resolve: {
+      userTasks: resolveUserTasks,
+    },
+  },
+  {
+    path: 'tasks/new',
+    component: NewTaskComponent,
+    canDeactivate: [canLeaveEditPage]
   },
 ];
