73834ecf7b2317cdcdd37f419bec2251623e0564
Brandon Ibañez <ibanezbrandonn@gmail.com>
2025-10-06 17:39:54 +0800

    Inline styles

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -2,7 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-server',
+  // templateUrl: './server.component.html',
   template: '<p>server works!</p>',
-  styleUrl: './server.component.css',
+  // styleUrl: './server.component.css',
+  styles: [
+    `
+      p {
+        color: blue;
+      }
+    `,
+  ],
 })
-export class ServerComponent { }
+export class ServerComponent {}
