7da04fe1d50e58593aa2bcb90dd27d07b88e51c8
Brandon Ibañez <ibanezbrandonn@gmail.com>
2025-10-11 14:50:59 +0800

    Section 19
    Course Project with Angular Basics
    Create needed components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
+import { Header } from './header/header';
+import { Recipes } from './recipes/recipes';
+import { RecipeList } from './recipes/recipe-list/recipe-list';
+import { RecipeDetail } from './recipes/recipe-detail/recipe-detail';
+import { RecipeItem } from './recipes/recipe-list/recipe-item/recipe-item';
+import { ShoppingList } from './shopping-list/shopping-list';
+import { ShoppingEdit } from './shopping-list/shopping-edit/shopping-edit';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ServerComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, Header, Recipes, RecipeList, RecipeDetail, RecipeItem, ShoppingList, ShoppingEdit],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { 
-
-}
+export class AppModule {}
