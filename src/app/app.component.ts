372aa1e78b0be970214f8a62df8843684cd70d50
Brandon Ibañez <ibanezbrandonn@gmail.com>
2025-10-17 22:00:21 +0800

    Section 23
    Directives Deep Dive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  loadedFeature = 'recipe';
-
-  onNavigate(feature: string) {
-    this.loadedFeature = feature;
-  }
+  numbers = [1, 2, 3, 4, 5];
+  onlyOdd = false;
 }
