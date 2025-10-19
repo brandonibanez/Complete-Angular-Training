8528e7d0137e93137e83d97c3a696658a98ad3f4
Brandon Ibañez <ibanezbrandonn@gmail.com>
2025-10-19 20:08:27 +0800

    Injecting Service into a Service
    @Injectable to enable dependency injection

--- src/app/accounts.service.ts
+++ src/app/accounts.service.ts
@@ -1,3 +1,4 @@
+import { LoggingService } from './logging.service';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -20,7 +21,7 @@ export class AccountsService {
     }
   ];
 
-  constructor() { }
+  constructor(private LoggingService: LoggingService) { }
 
   addAccount(name: string, status: string) {
     this.accounts.push({name: name, status: status});
