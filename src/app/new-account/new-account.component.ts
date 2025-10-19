fd61028a4f7974e9d2dc8e5eb08378885de833db
Brandon Ibañez <ibanezbrandonn@gmail.com>
2025-10-19 20:16:53 +0800

    Using Services for Cross-Component Communication

--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -9,10 +9,12 @@ import { Component, EventEmitter, Output } from '@angular/core';
 })
 export class NewAccountComponent {
 
-  constructor(private loggingService: LoggingService, private accountsService: AccountsService) { }
+  constructor(private loggingService: LoggingService, private accountsService: AccountsService) { 
+    this.accountsService.statusUpdated.subscribe((status: string) => alert('New Status: ' + status));
+  }
 
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountsService.addAccount(accountName, accountStatus);
-    this.loggingService.logStatusChange(accountStatus);
+    // this.loggingService.logStatusChange(accountStatus);
   }
 }
