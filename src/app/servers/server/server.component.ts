976cdc4571de74b954e07571fce610cb0787e7ff
Brandon Ibañez <ibanezbrandonn@gmail.com>
2025-10-28 16:45:13 +0800

    Section 28
    Course Project - Routing

--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
-import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-server',
@@ -11,20 +10,10 @@ import { ActivatedRoute, Router } from '@angular/router';
 export class ServerComponent implements OnInit {
   server: {id: number, name: string, status: string};
 
-  constructor(private serversService: ServersService, private route: ActivatedRoute, private router: Router) { }
+  constructor(private serversService: ServersService) { }
 
   ngOnInit() {
-    this.route.data.subscribe( data => {
-      this.server = data['server'];
-    });
-    // this.server = this.serversService.getServer(+this.route.snapshot.params['id']);
-    // this.route.params.subscribe(params => {
-    //   this.server = this.serversService.getServer(+params['id']);
-    // });
-  }
-
-  onEdit() {
-    this.router.navigate(['edit'], { relativeTo: this.route, queryParamsHandling: 'preserve' });
+    this.server = this.serversService.getServer(1);
   }
 
 }
--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,31 +1,16 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { Subscription } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit, OnDestroy {
+export class UserComponent implements OnInit {
   user: {id: number, name: string};
-  subscription: Subscription;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor() { }
 
   ngOnInit() {
-    this.user = {
-      id: this.route.snapshot.params['id'],
-      name: this.route.snapshot.params['name']
-    };
-    this.subscription = this.route.params.subscribe(params => {
-      this.user.id = params['id'];
-      this.user.name = params['name'];
-    });
-  }
-
-  ngOnDestroy(): void {
-    this.subscription.unsubscribe();
   }
 
 }
