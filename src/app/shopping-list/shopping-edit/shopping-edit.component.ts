2d5487ec1272604ac6fbdd57b2fdd12313a6aed6
Brandon Ibañez <ibanezbrandonn@gmail.com>
2025-11-08 14:59:31 +0800

    Adding Validation to the Form

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -26,6 +26,7 @@ export class ShoppingEditComponent implements OnInit {
     const ingAmount = form.value.amount;
     const newIngredient = new Ingredient(ingName, ingAmount);
     this.slService.addIngredient(newIngredient);
+    form.resetForm();
   }
 
 }
