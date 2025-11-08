3c6f3844b9c95c39f044d70be425824f8bc501d8
Brandon Ibañez <ibanezbrandonn@gmail.com>
2025-11-08 14:46:22 +0800

    Section 32
    Course Project - Forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,79 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
-import { Observable } from 'rxjs/internal/Observable';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  genders = ['male', 'female'];
-  form: FormGroup;
-  invalidUsernames = ['Chris', 'Anna'];
-
-  ngOnInit() {
-    this.form = new FormGroup({
-      'userData': new FormGroup({
-        'username': new FormControl(null, [Validators.required, this.invalidNames.bind(this)]),
-        'email': new FormControl(null, [Validators.required, Validators.email], this.invalidEmails.bind(this)),
-      }),
-      'gender': new FormControl('male'),
-      'hobbies': new FormArray([]),
-    });
-    this.form.valueChanges.subscribe(value => {
-      console.log(value);
-    });
-    this.form.statusChanges.subscribe(status => {
-      console.log(status);
-    });
-    this.form.setValue({
-      'userData': {
-        'username': 'Max',
-        'email': 'max@test.com',
-      },
-      'gender': 'male',
-      'hobbies': [],
-    });
-    this.form.patchValue({
-      'userData': {
-        'username': 'Anna',
-      },
-    });
-  }
-
-  onSubmit() {
-    console.log(this.form);
-    this.form.reset();
-  }
-
-  onAddHobby() {
-    const control = new FormControl(null, Validators.required);
-    (<FormArray>this.form.get('hobbies')).push(control);
-  }
-
-  get controls() {
-    return (this.form.get('hobbies') as FormArray).controls;
-  }
-
-  invalidNames(control: FormControl): {[s: string]: boolean} {
-    if (this.invalidUsernames.indexOf(control.value) !== -1) {
-      return {'nameIsInvalid': true};
-    }
-    return null;
-  }
-
-  invalidEmails(control: FormControl): Promise<any> | Observable<any> {
-    const promise = new Promise<any>((resolve, reject) => {
-      setTimeout(() => {
-        if (control.value === 'test@test.com') {
-          resolve({'emailIsInvalid': true});
-        } else {
-          resolve(null);
-        }
-      }, 1500);
-    });
-    return promise;
-  }
-    
+export class AppComponent {
 }
