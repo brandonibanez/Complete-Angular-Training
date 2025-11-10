a1e46449d62a82ac52e9ef337cfb1f6504b6016b
Brandon Ibañez <ibanezbrandonn@gmail.com>
2025-11-10 22:23:30 +0800

    Using catchError Operator

--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -1,8 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Post } from './post.model';
-import { map } from 'rxjs/operators';
-import { Subject } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+import { Subject, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -34,7 +34,9 @@ export class PostsService {
           }
         }
         return postsArray;
-      }));
+      }, catchError(errorRes => {
+        return throwError(errorRes);
+      })));
   }
 
   clearPosts() {
