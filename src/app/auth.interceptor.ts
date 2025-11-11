b8ea0d9a0af7ce3f15ed48172b81af03ac98fcb8
Brandon Ibañez <ibanezbrandonn@gmail.com>
2025-11-11 19:56:16 +0800

    Response Interceptors

--- src/app/auth.interceptor.ts
+++ src/app/auth.interceptor.ts
@@ -3,9 +3,11 @@ import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
-  HttpInterceptor
+  HttpInterceptor,
+  HttpEventType
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
@@ -16,8 +18,10 @@ export class AuthInterceptor implements HttpInterceptor {
     const modifiedReq = request.clone({
       headers: request.headers.append('Auth', 'xyz')
     });
-    return next.handle(modifiedReq);
-    //
-    return next.handle(request);
+    return next.handle(modifiedReq).pipe(tap(event => {
+      if(event.type === HttpEventType.Response) {
+        console.log('Response received');
+      }
+    }));
   }
 }
