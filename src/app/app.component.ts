8ecca88f1cff0b5c7dff2993a158763796ddd085
Brandon Ibañez <ibanezbrandonn@gmail.com>
2025-11-12 17:16:18 +0800

    Section 36
    Authentication & Route Protection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,63 +1,14 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs/operators';
-import { Post } from './post.model';
-import { PostsService } from './posts.service';
-import { Subscription } from 'rxjs';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit, OnDestroy {
-  loadedPosts: Post[] = [];
-  isFetching = false;
-  error = null;
-  private subscription: Subscription;
+export class AppComponent {
+  loadedFeature = 'recipe';
 
-  constructor(private http: HttpClient, private postsService: PostsService) { }
-
-  ngOnInit() {
-    this.subscription = this.postsService.error.subscribe(errorMessage => {
-      this.error = errorMessage;
-    });
-
-    this.isFetching = true;
-    this.postsService.fetchPosts().subscribe(posts => {
-      this.isFetching = false;
-      this.loadedPosts = posts;
-    }, error => {
-      this.isFetching = false;
-      this.error = error.message;
-    });
-  }
-
-  ngOnDestroy() {
-    this.subscription.unsubscribe();
-  }
-
-  onCreatePost(postData: Post) {
-    // Send Http request
-    this.postsService.createAndStorePost(postData.title, postData.content);
-  }
-
-  onFetchPosts() {
-    // Send Http request
-    this.isFetching = true;
-    this.postsService.fetchPosts().subscribe(posts => {
-      this.isFetching = false;
-      this.loadedPosts = posts;
-    }, error => {
-      this.isFetching = false;
-      this.error = error.message;
-    });
-  }
-
-  onClearPosts() {
-    // Send Http request
-    this.postsService.clearPosts().subscribe(() => {
-      this.loadedPosts = [];
-    });
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
   }
 }
