67fa60dc2bde0928d11703c62f014d5b8d79ccf3
Brandon Ibañez <ibanezbrandonn@gmail.com>
2025-11-17 22:04:26 +0800

    Starter Project - Routing with Standalone Components

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,16 @@
 import { enableProdMode } from '@angular/core';
-
-import { environment } from './environments/environment';
 import { bootstrapApplication } from '@angular/platform-browser';
+
 import { AppComponent } from './app/app.component';
-import { AnalyticsService } from './app/shared/analytics.service';
+// import { AnalyticsService } from './app/shared/analytics.service';
+import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
 }
+
 bootstrapApplication(AppComponent, {
-  providers: [AnalyticsService],
+  providers: [
+    // AnalyticsService,
+  ],
 });
