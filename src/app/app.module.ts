856a9a5f38b3485f51767f1e6d84826ded6ea712
Brandon Ibañez <ibanezbrandonn@gmail.com>
2025-11-17 21:06:49 +0800

    Section 39
    Standalone Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { AppRoutingModule } from './app-routing.module';
-import { ShoppingListModule } from './shopping-list/shopping-list-module';
-import { SharedModule } from './shared/shared-module';
-import { CoreModule } from './core-module';
-import { AuthModule } from './auth/auth-module';
+import { SharedModule } from './shared/shared.module';
+import { DetailsComponent } from './welcome/details/details.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-  ],
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-    AppRoutingModule,
-    ShoppingListModule,
-    SharedModule,
-    CoreModule,
-    AuthModule
-  ],
+  declarations: [AppComponent, WelcomeComponent, DetailsComponent],
+  imports: [BrowserModule, SharedModule],
+  providers: [],
   bootstrap: [AppComponent],
-
 })
 export class AppModule {}
