f2d72c69b511ec3a3bd0356a9b368b4f37f34cb9
Brandon Ibañez <ibanezbrandonn@gmail.com>
2025-11-19 21:22:23 +0800

    Reading & Outputting a Signal Value

---
+++ src/app/signals/signals.component.ts
@@ -0,0 +1,24 @@
+import { NgFor } from '@angular/common';
+import { Component, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-signals',
+  templateUrl: './signals.component.html',
+  styleUrls: ['./signals.component.css'],
+  standalone: true,
+  imports: [NgFor]
+})
+export class SignalsComponent {
+  actions: string[] = [];
+  counter = signal(0);
+
+  increment() {
+    this.counter.update(value => value + 1);
+    this.actions.push('INCREMENT');
+  }
+
+  decrement() {
+    this.counter.update(value => value - 1);
+    this.actions.push('DECREMENT');
+  }
+}
