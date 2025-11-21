f3cc35fd5e2580ec8bde1e4648c86ee38b0d5221
Brandon Ibañez <ibanezbrandonn@gmail.com>
2025-11-21 20:48:54 +0800

    Attaching Data to Actions

--- src/app/store/counter.reducer.ts
+++ src/app/store/counter.reducer.ts
@@ -3,4 +3,4 @@ import { increment } from "./counter.actions";
 
 const initialState = 0;
 
-export const counterReducer = createReducer(initialState, on(increment, (state) => state + 1));
+export const counterReducer = createReducer(initialState, on(increment, (state, action) => state + action.value));
