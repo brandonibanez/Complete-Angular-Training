2752c8087d6aaa91340548c2ffc8004514a5f4f8
Brandon Ibañez <ibanezbrandonn@gmail.com>
2025-11-21 20:42:14 +0800

    Dispatching Actions

--- src/app/counter-output/counter-output.component.ts
+++ src/app/counter-output/counter-output.component.ts
@@ -1,9 +1,7 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Observable, Subscription } from 'rxjs';
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 
-import { CounterService } from '../counter.service';
 import { Store } from '@ngrx/store';
-import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-counter-output',
--- src/app/store/counter.reducer.ts
+++ src/app/store/counter.reducer.ts
@@ -4,7 +4,3 @@ import { increment } from "./counter.actions";
 const initialState = 0;
 
 export const counterReducer = createReducer(initialState, on(increment, (state) => state + 1));
-
-// export function counterReducer(state = initialState) {
-//     return state;
-// }
