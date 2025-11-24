331c335a7007d3966e39f233ee798f5a131a6931
Brandon Ibañez <ibanezbrandonn@gmail.com>
2025-11-24 19:47:21 +0800

    Section 43
    Adding Offline Capabilities with Service Workers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,100 +1,21 @@
-import { Component, trigger } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Post } from './post.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  animations: [
-    trigger('divState', [
-      state('normal', style({
-        'background-color': 'red',
-        transform: 'translateX(0)'
-      })),
-      state('highlighted', style({
-        'background-color': 'blue',
-        transform: 'translateX(100px)'
-      })),
-      transition('normal <=> highlighted', animate(300)),
-    ]),
-    trigger('wildState', [
-      state('normal', style({
-        'background-color': 'red',
-        transform: 'translateX(0) scale(1)'
-      })),
-      state('highlighted', style({
-        'background-color': 'blue',
-        transform: 'translateX(100px) scale(1)'
-      })),
-      state('shrunken', style({
-        'background-color': 'green',
-        transform: 'translateX(0) scale(0.5)'
-      })),
-      transition('normal => highlighted', animate(300)),
-      transition('highlighted <=> normal', animate(800)),
-      transition('shrunken <=> *', [
-        style({
-          'background-color': 'orange'
-        }),
-        animate(1000, style({
-          borderRadius: '50px'
-        })),
-        animate(500)
-      ])
-    ]),
-    trigger('list1', [
-      state('in', style({
-        opacity: 1,
-        transform: 'translateX(0)'
-      })),
-      transition('*void => *', [
-        style({
-          opacity: 0,
-          transform: 'translateX(-100px)'
-        }),
-        animate(300)
-      ]),
-    ]),
-    trigger('list2', [
-      state('in', style({
-        opacity: 1,
-        transform: 'translateX(0)'
-      })),
-      transition('*void => *', [
-        animate(1000, keyframes([
-          style({
-            opacity: 0,
-            transform: 'translateX(-100px)',
-            offset: 0
-          }),
-          style({
-            opacity: 0.5,
-            transform: 'translateX(-50px)',
-            offset: 0.5
-          }),
-          style({
-            opacity: 1,
-            transform: 'translateX(-20px)',
-            offset: 1
-          }),
-        ]))
-      ]),
-    ])
-  ]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  state = 'normal';
-  wildState = 'normal';
-  list = ['Milk', 'Sugar', 'Bread'];
+export class AppComponent implements OnInit {
+  posts: Post[] = [];
 
-  onAdd(item) {
-    this.list.push(item);
-  }
+  constructor(private http: HttpClient) {}
 
-  onAnimate() {
-    this.state === 'normal' ? this.state = 'highlighted' : this.state = 'normal';
-    this.wildState === 'normal' ? this.wildState = 'highlighted' : this.wildState = 'normal';
-  }
-
-  onShrink() {
-    this.wildState = 'shrunken';
+  ngOnInit() {
+    this.http
+      .get<Post[]>('https://jsonplaceholder.typicode.com/posts')
+      .subscribe(fetchedPosts => (this.posts = fetchedPosts));
   }
 }
