8c3bbca8884c53ead666a36112117933ae969e9e
Brandon Ibañez <ibanezbrandonn@gmail.com>
2026-02-13 16:13:17 +0800

    Reducer function without using createReducer

--- src/app/counter-store/counter.reducer.ts
+++ src/app/counter-store/counter.reducer.ts
@@ -3,12 +3,19 @@ import { decrement, increment } from "./counter.actions";
 
 const initialState = 0;
 
-export const counterReducer = createReducer(
-    initialState,
-    on(increment, (state, { amount }) => state + amount),
-    on(decrement, state => state - 1)
-);
+// export const counterReducer = createReducer(
+//     initialState,
+//     on(increment, (state, { amount }) => state + amount),
+//     on(decrement, state => state - 1)
+// );
 
-// export function counterReducer(state = initialState) {
-//     return initialState;
-// }
+export function counterReducer(state = initialState, action: any) {
+    switch (action.type) {
+        case increment.type:
+            return state + action.amount;
+        case decrement.type:
+            return state - 1;
+        default:
+            return state;
+    }
+}
