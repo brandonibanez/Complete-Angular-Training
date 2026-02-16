ba859009cba08fb264742297e97e5b05e2bec59c
Brandon Ibañez <ibanezbrandonn@gmail.com>
2026-02-16 18:04:28 +0800

    Mapping Form in the View

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,11 @@
-import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
-import { filter } from 'rxjs-compat/operator/filter';
-import { map } from 'rxjs/operators';
+import { Component } from '@angular/core';
+
+import { LoginComponent } from './auth/login/login.component';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  imports: [LoginComponent],
 })
-export class AppComponent implements OnInit {
-  constructor(private http: HttpClient) {
-    http.get<any>('https://api.thecatapi.com/v1/images/search?limit=10').pipe(
-      map(response => response.filter(item => item.width > 500)))
-      .subscribe((response) => {
-        console.log(response);
-      });
-  }
-
-  ngOnInit() { }
-}
+export class AppComponent {}
---
+++ src/app/auth/login/login.component.ts
@@ -0,0 +1,21 @@
+import { OnEndResult } from './../../../../node_modules/esbuild/lib/main.d';
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css',
+})
+export class LoginComponent {
+  form = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl(''),
+  });
+
+  onSubmit() {
+    console.log(this.form.value);
+  }
+}
