144de8776f63e9f627a9c9628dc77ebd1483288c
Brandon Ibañez <ibanezbrandonn@gmail.com>
2026-05-12 03:57:21 +0800

    Include interceptor to HttpClient

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { authInterceptorInterceptor } from './app/auth/login/auth-interceptor.interceptor';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideHttpClient() // Essential for injection to work
+    provideHttpClient(
+      withInterceptors([
+        authInterceptorInterceptor
+      ])
+    ) // Essential for injection to work
   ]
 }).catch((err) => console.error(err));
