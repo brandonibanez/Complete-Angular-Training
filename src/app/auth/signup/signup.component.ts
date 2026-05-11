a8b0ea44f8c86583d6612105c33d18e9ed0a061d
Brandon Ibañez <ibanezbrandonn@gmail.com>
2026-05-11 18:59:28 +0800

    Use testService to fetch state value

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,9 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TestService } from '../service/test.service';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
+  imports: [AsyncPipe],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css',
 })
-export class SignupComponent {}
+export class SignupComponent implements OnInit{
+
+  constructor(private testService: TestService) {}
+
+  candidate$ = this.testService.candidate$;
+
+  ngOnInit(): void {
+    // this.testService.candidate$.subscribe(res => {
+    //   this.candidate = res
+    // })
+  }
+
+}
