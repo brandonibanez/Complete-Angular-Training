8c95f27f048071b99ce6348098d0833a95aa3947
Brandon Ibañez <ibanezbrandonn@gmail.com>
2026-05-11 19:01:10 +0800

    Updates on login component
    - Use testService for state management
    - Use HttpClient to call REST API
    - Test reactivity through event binding and change Behavior Subject value

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,6 +1,10 @@
-import { OnEndResult } from './../../../../node_modules/esbuild/lib/main.d';
-import { Component } from '@angular/core';
-import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { map, Observable, throwError } from 'rxjs';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Candidate } from './candidate';
+import { TestService } from '../service/test.service';
+import { AsyncPipe } from '@angular/common';
 
 function mustContainQuestionMark(control: AbstractControl) {
   if (control.value.includes('?')) {
@@ -22,18 +26,46 @@ function mustBeSame(control: AbstractControl) {
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, FormsModule, AsyncPipe],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  private http = inject(HttpClient);
+  candidate$ = this.testService.candidate$;
+
   form = new FormGroup({
     email: new FormControl('', { validators: [Validators.required, Validators.email] }),
-    password: new FormControl('', { validators: [Validators.required, Validators.minLength(6), mustContainQuestionMark] }),
+    password: new FormControl('', { validators: [Validators.required] }),
+    // password: new FormControl('', { validators: [Validators.required, Validators.minLength(6), mustContainQuestionMark] }),
     confirmPassword: new FormControl('', { validators: [Validators.required] }),
   }, { validators: [mustBeSame] });
 
+  ngOnInit() {
+    setTimeout(() => {
+      this.testService.updateUser('SUPER BOOGER!');
+    }, 1000);
+  }
+
+  getCandidates(): Observable<Candidate[]> {
+    return this.http.get<Candidate[]>("http://localhost:8080/candidates").pipe(map(a => a.filter(a => a.name == "John")));
+  }
+
+  constructor(private testService: TestService) { }
+
   onSubmit() {
     console.log(this.form.value);
+    this.getCandidates().subscribe({
+      next: a => {
+        console.log(a);
+        this.testService.updateUser(a[0].name);
+      },
+      error: a => console.log(`Object is empty! ${a}`),
+      complete: () => console.log("It is done!")
+    });
+  }
+
+  test(val: string) {
+    this.testService.updateUser(val);
   }
 }
