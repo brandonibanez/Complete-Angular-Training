db9e29ae94bd163273c6e7e153305c367e3f8b79
Brandon Ibañez <ibanezbrandonn@gmail.com>
2026-05-12 22:40:20 +0800

    Provide routes in main ts

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,13 @@ import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { authInterceptorInterceptor } from './app/auth/login/auth-interceptor.interceptor';
+import { authInterceptorInterceptor } from './app/core/auth/auth-interceptor.interceptor';
+import { provideRouter } from '@angular/router';
+import { routes } from './app/app.routes';
 
 bootstrapApplication(AppComponent, {
   providers: [
+    provideRouter(routes),
     provideHttpClient(
       withInterceptors([
         authInterceptorInterceptor
