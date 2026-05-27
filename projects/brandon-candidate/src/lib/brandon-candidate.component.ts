bd85d577b65d6a4b9d39fd28c1a4af97fbf11a48
Brandon Ibañez <ibanezbrandonn@gmail.com>
2026-05-27 19:38:28 +0800

    Simple custom library

---
+++ projects/brandon-candidate/src/lib/brandon-candidate.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-brandon-candidate',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      brandon-candidate works!
+    </p>
+  `,
+  styles: ``
+})
+export class BrandonCandidateComponent {
+
+}
---
+++ projects/brandon-candidate/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of brandon-candidate
+ */
+
+export * from './lib/brandon-candidate.service';
+// export * from './lib/brandon-candidate.component';
+export * from './lib/types/candidate';
